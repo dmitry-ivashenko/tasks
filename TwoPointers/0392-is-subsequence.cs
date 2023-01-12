ddeda7f3d8f655d696040ee4b9793a6a8bb1cb87
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-01-12 14:09:51 +0100

    Create 0392-is-subsequence.cs

---
+++ TwoPointers/0392-is-subsequence.cs
@@ -0,0 +1,27 @@
+
+// 392. Is Subsequence
+// https://leetcode.com/problems/is-subsequence/
+
+public class Solution
+{
+    public bool IsSubsequence(string s, string t)
+    {
+        int i = 0;
+        int j = 0;
+        while ((i < s.Length) && (j < t.Length))
+        {
+            if (s[i] == t[j])
+            {
+                i += 1;
+            }
+            j += 1;
+
+        }
+        if (i == s.Length)
+        {
+            return true;
+        }
+        else return false;
+    }
+}
+        
