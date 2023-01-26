1c9cc0defcc82204813e310a1065da475f7e596b
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-01-26 11:32:50 +0100

    Create 1448-count-good-nodes-in-binary-tree.cs

---
+++ Tree/1448-count-good-nodes-in-binary-tree.cs
@@ -0,0 +1,35 @@
+
+// 1448. Count Good Nodes in Binary Tree
+// https://leetcode.com/problems/count-good-nodes-in-binary-tree/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private int goodNodeCount = 0;
+    public void dfs(TreeNode cur, int pathMax) {
+        if(cur == null) return;
+        if(cur.val >= pathMax) {
+            pathMax = cur.val;
+            goodNodeCount++;
+        }
+        dfs(cur.left, pathMax);
+        dfs(cur.right, pathMax);
+        
+    }
+    public int GoodNodes(TreeNode root) {
+        dfs(root, int.MinValue);
+        return goodNodeCount;
+    }
+}
+        
