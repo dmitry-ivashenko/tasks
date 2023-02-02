c74fffb8b98f2abf8fdb60cfcfbb5989be64fd46
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-02-02 07:53:06 +0100

    Create 0001-two-sum.cs

---
+++ Array/0001-two-sum.cs
@@ -0,0 +1,21 @@
+
+// 1. Two Sum
+// https://leetcode.com/problems/two-sum/
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        Dictionary<int, int> indices = new Dictionary<int, int>();
+
+        for (int i = 0; i < nums.Length; i++) {
+            var diff = target - nums[i];
+            if (indices.ContainsKey(diff)) {
+                return new int[] {indices[diff], i};
+            }
+            indices[nums[i]] = i;
+        }
+        return null;
+    }
+}
+
+
+        
