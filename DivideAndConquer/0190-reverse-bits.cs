1f950db566f5b4f814e37f5d06e928c02174cb4f
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-02-09 05:27:52 +0100

    Create 0190-reverse-bits.cs

---
+++ DivideAndConquer/0190-reverse-bits.cs
@@ -0,0 +1,20 @@
+
+// 190. Reverse Bits
+// https://leetcode.com/problems/reverse-bits/
+
+public class Solution 
+{
+    public uint reverseBits(uint n) 
+    {
+		uint result = 0;
+		for (int i = 0; i < 32; i++)
+		{
+			uint temp = (n & 1);
+			result = (result << 1) + temp;
+			n >>= 1;
+		}
+        return result;
+    } 
+}
+
+        
