226bbdbf009315d38ef13637b9c91e6a7ba26f66
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-02-14 00:28:49 +0100

    Create 0303-range-sum-query-immutable.cs

---
+++ Array/0303-range-sum-query-immutable.cs
@@ -0,0 +1,21 @@
+
+// 303. Range Sum Query - Immutable
+// https://leetcode.com/problems/range-sum-query-immutable/
+
+public class NumArray {
+    private int[] nums;
+    public NumArray(int[] nums) {
+        this.nums = nums;
+    }
+    
+    public int SumRange(int left, int right) {
+        int sum = 0;
+        for (int i = left; i <= right; i++)
+        {
+            sum += this.nums[i];
+        }
+        return sum;
+    }
+}
+
+        
