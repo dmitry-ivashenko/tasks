5c87cc647d7990e7df65126603f878410375434c
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-02-26 15:40:04 +0100

    Create 0560-subarray-sum-equals-k.cs

---
+++ Array/0560-subarray-sum-equals-k.cs
@@ -0,0 +1,36 @@
+
+// 560. Subarray Sum Equals K
+// https://leetcode.com/problems/subarray-sum-equals-k/
+
+public class Solution
+{
+    public int SubarraySum(int[] nums, int k)
+    {
+        Dictionary<int, int> prefixToCounts = new Dictionary<int, int>();
+        prefixToCounts.Add(0, 1);
+
+        int sum = 0;
+        int res = 0;
+
+        for (int i = 0; i < nums.Length; i++)
+        {
+            sum += nums[i];
+            int diff = sum - k;
+
+            prefixToCounts.TryGetValue(diff, out int count);
+
+            res += count;
+
+            if (prefixToCounts.ContainsKey(sum))
+            {
+                prefixToCounts[sum] += 1;
+                continue;
+            }
+
+            prefixToCounts.Add(sum, 1);
+        }
+
+        return res;
+    }
+}
+        
