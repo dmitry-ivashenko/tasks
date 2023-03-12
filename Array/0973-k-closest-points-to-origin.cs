92d60868ad2afb9a682b12838fe9d3b92c0b56dd
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-03-12 21:11:45 +0100

    Create 0973-k-closest-points-to-origin.cs

---
+++ Array/0973-k-closest-points-to-origin.cs
@@ -0,0 +1,27 @@
+
+// 973. K Closest Points to Origin
+// https://leetcode.com/problems/k-closest-points-to-origin/
+
+public class Solution {
+    public int[][] KClosest(int[][] points, int k) {
+        var items = points.Select(point => {
+            long x = point[0];
+            long y = point[1];
+
+            return (point, x * x + y * y);
+        });
+
+        int[][] result = new int[k][];
+        // T: O(n)
+        PriorityQueue<int[], long> queue = new(items);
+
+        // T: O(k log(n))
+        for (int i = 0; i < k; i++) {
+            result[i] = queue.Dequeue();
+        }
+
+        return result;
+    }
+}
+
+        
