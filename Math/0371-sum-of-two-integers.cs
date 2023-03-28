00255d6c91f6fcba88a4f49b67f5f97c72d8d27b
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-03-28 06:52:43 +0200

    Create 0371-sum-of-two-integers.cs

---
+++ Math/0371-sum-of-two-integers.cs
@@ -0,0 +1,16 @@
+
+// 371. Sum of Two Integers
+// https://leetcode.com/problems/sum-of-two-integers/
+
+public class Solution {
+    public int GetSum(int a, int b) {
+        while (b != 0)
+        {
+            var carry = a & b;
+            a = a ^ b;
+            b = carry << 1;
+        }
+        return a;
+    }
+}
+        
