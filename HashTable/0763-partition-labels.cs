dc84f09938303fe5ca02a156d8c75acbfbe449ca
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-05-01 06:27:00 +0200

    Create 0763-partition-labels.cs

---
+++ HashTable/0763-partition-labels.cs
@@ -0,0 +1,34 @@
+
+// 763. Partition Labels
+// https://leetcode.com/problems/partition-labels/
+
+public class Solution
+{
+    public IList<int> PartitionLabels(string s)
+    {
+
+        var chars = new int[26];
+        for (var ch = 0; ch < s.Length; ch++)
+        {
+            chars[s[ch] - 'a'] = ch;
+        }
+
+        var result = new List<int>();
+        var end = 0;
+        var size = 0;
+        for (var i = 0; i < s.Length; i++)
+        {
+            end = Math.Max(chars[s[i] - 'a'], end);
+            size++;
+            if (i == end)
+            {
+                result.Add(size);
+                size = 0;
+            }
+
+        }
+
+        return result;
+    }
+}
+        
