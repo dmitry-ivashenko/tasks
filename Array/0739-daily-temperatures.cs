c2d0ca7ee309e24c82b319ca7bab417a20693146
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-05-07 00:35:25 +0200

    Create 0739-daily-temperatures.cs

---
+++ Array/0739-daily-temperatures.cs
@@ -0,0 +1,27 @@
+
+// 739. Daily Temperatures
+// https://leetcode.com/problems/daily-temperatures/
+
+public class Solution
+{
+    public int[] DailyTemperatures(int[] temperatures)
+    {
+        var result = new int[temperatures.Length];
+        Array.Fill(result, 0);
+        var stack = new Stack<int>();
+
+        for (var i = 0; i < temperatures.Length; i++)
+        {
+            var t = temperatures[i];
+            while (stack.Any() && temperatures[stack.Peek()] < t)
+            {
+                var prev = stack.Pop();
+                result[prev] = i - prev;
+            }
+            stack.Push(i);
+        }
+
+        return result;
+    }
+}
+        
