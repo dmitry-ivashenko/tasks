cd47f4ee97fa82321442005b85a9c01fcb6a4604
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-05-08 09:27:34 +0200

    Create 0206-reverse-linked-list.cs

---
+++ LinkedList/0206-reverse-linked-list.cs
@@ -0,0 +1,33 @@
+
+// 206. Reverse Linked List
+// https://leetcode.com/problems/reverse-linked-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode prev = null, curr = head;
+
+        while (curr != null)
+        {
+            var temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
+
+        
