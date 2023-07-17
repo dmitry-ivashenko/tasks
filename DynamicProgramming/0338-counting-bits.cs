74e417c25d6606792fc4caa2261c7da7b3ee0378
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-07-17 15:19:38 +0200

    Create 0338-counting-bits.cs

---
+++ DynamicProgramming/0338-counting-bits.cs
@@ -0,0 +1,21 @@
+
+// 338. Counting Bits
+// https://leetcode.com/problems/counting-bits/
+
+public class Solution {
+    public int[] CountBits(int n) {
+        var hammingWeights = new int[n+1];
+        for (int i = 0; i <= n; i++)
+        {
+            var binary = Convert.ToString(i, 2);
+            var hammingWeight = 0;
+            for (int j = 0; j < binary.Length; j++)
+            {
+                hammingWeight += binary[j] - '0';
+            }
+            hammingWeights[i] = hammingWeight;
+        }
+        return hammingWeights;
+    }
+}
+        
