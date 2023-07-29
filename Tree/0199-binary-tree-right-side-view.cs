dbf1922de1ae3547fcdf24bb02460e7cefca9662
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-07-29 11:56:05 +0200

    Create 0199-binary-tree-right-side-view.cs

---
+++ Tree/0199-binary-tree-right-side-view.cs
@@ -0,0 +1,36 @@
+
+// 199. Binary Tree Right Side View
+// https://leetcode.com/problems/binary-tree-right-side-view/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    private List<int> _result = new();
+
+    public IList<int> RightSideView(TreeNode root) {
+        Dfs(root, 0);
+        return _result;
+    }
+
+    private void Dfs(TreeNode root, int level) {
+        if (root == null) return;
+        if (level >= _result.Count) _result.Add(root.val);
+
+        // At first visit right node
+        Dfs(root.right, level + 1);
+        Dfs(root.left, level + 1);
+    }
+}
+
+        
