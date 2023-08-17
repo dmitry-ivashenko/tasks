dae515244e2e19f6acf8c9a0a5527b01d48c56d2
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-08-17 05:14:15 +0200

    Create 0202-happy-number.cs

---
+++ HashTable/0202-happy-number.cs
@@ -0,0 +1,32 @@
+
+// 202. Happy Number
+// https://leetcode.com/problems/happy-number/
+
+public class Solution {
+    public bool IsHappy(int n) {
+        
+        var set = new HashSet<int>();
+
+        while (!set.Contains(n))
+        {
+            set.Add(n);
+            n = SumOfSquare(n);
+            if (n == 1) return true;
+        }
+        
+        return false;
+    }
+    
+    int SumOfSquare (int x)
+    {
+        int sum = 0;
+        
+        for (int i = x; i > 0; i /= 10){
+            int y = i % 10;
+            sum += y * y;
+        }
+        
+        return sum;
+    }
+}
+        
