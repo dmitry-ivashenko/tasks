0fd3fcc46b25542f6d7276b4b6643c324164a727
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-09-15 09:39:03 +0200

    Create 0049-group-anagrams.cs

---
+++ Array/0049-group-anagrams.cs
@@ -0,0 +1,24 @@
+
+// 49. Group Anagrams
+// https://leetcode.com/problems/group-anagrams/
+
+public class Solution {
+    public IList<IList<string>> GroupAnagrams(string[] strs) {
+        var groups = new Dictionary<string, IList<string>>();
+
+        foreach (string s in strs) {
+            char[] hash = new char[26];
+            foreach (char c in s) {
+                hash[c - 'a']++;
+            }
+            
+            string key = new string(hash);
+            if (!groups.ContainsKey(key)) {
+                groups[key] = new List<string>();
+            }
+            groups[key].Add(s);
+        }
+        return groups.Values.ToList();
+    }
+}
+        
