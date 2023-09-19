74abadd1a1646d5be7a444aeda70d24cc21ea11f
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-09-19 18:36:33 +0200

    Create 0703-kth-largest-element-in-a-stream.cs

---
+++ Tree/0703-kth-largest-element-in-a-stream.cs
@@ -0,0 +1,24 @@
+
+// 703. Kth Largest Element in a Stream
+// https://leetcode.com/problems/kth-largest-element-in-a-stream/
+
+public class KthLargest {
+    PriorityQueue<int, int> data = new PriorityQueue<int, int>();
+    int k;
+    
+    public KthLargest(int k, int[] nums) {
+        this.k = k;
+        foreach(var num in nums)
+            Add(num);
+    }
+    
+    public int Add(int val) {
+        data.Enqueue(val, val);
+        
+        if(data.Count > k) 
+            data.Dequeue();
+        
+        return data.Peek();
+    }
+}
+        
