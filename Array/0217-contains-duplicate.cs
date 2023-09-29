146028b1e18a0ae6a81a2defb4c67ac508448f11
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-09-29 12:42:50 +0200

    Create 0217-contains-duplicate.cs

---
+++ Array/0217-contains-duplicate.cs
@@ -0,0 +1,16 @@
+
+// 217. Contains Duplicate
+// https://leetcode.com/problems/contains-duplicate/
+
+public class Solution {
+    public bool ContainsDuplicate(int[] nums) {
+        HashSet<int> set = new HashSet<int>();
+        
+        foreach (int x in nums){
+            if (set.Contains(x)) return true;
+            set.Add(x);
+        }
+        return false;
+    }
+}
+        
