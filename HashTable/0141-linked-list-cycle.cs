1cc86f86afe40029f20642c10f27ac17802f689e
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-09-30 20:33:34 +0200

    Create 0141-linked-list-cycle.cs

---
+++ HashTable/0141-linked-list-cycle.cs
@@ -0,0 +1,31 @@
+
+// 141. Linked List Cycle
+// https://leetcode.com/problems/linked-list-cycle/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+
+public class Solution {
+    public bool HasCycle(ListNode head) {
+        ListNode slow = head, fast = head;
+
+        while (fast != null && fast.next != null)
+        {
+            fast = fast.next.next;
+            slow = slow.next;
+            if (slow.Equals(fast)) return true;
+        }
+        return false;
+    }
+}
+
+        
