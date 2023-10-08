5e84fb15f797b383417cb4ff0fb54bbdb9fbbe6a
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-10-08 21:11:05 +0200

    Create 0167-two-sum-ii-input-array-is-sorted.cs

---
+++ Array/0167-two-sum-ii-input-array-is-sorted.cs
@@ -0,0 +1,25 @@
+
+// 167. Two Sum II - Input Array Is Sorted
+// https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
+
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        // Using 2 pointers. Since sorted, if l+r > target, decrease r.
+        // Else if l+r < target, increase l. Else, result is found.
+        int left = 0, right = numbers.Length - 1;
+
+        while (left < right) {
+            int sum = numbers[left] + numbers[right];
+            if (sum > target) {
+                right--;
+            } else if (sum < target) {
+                left++;
+            } else {
+                return new int[] {left + 1, right + 1};
+            }
+        }
+
+        return new int[0];
+    }
+}
+        
