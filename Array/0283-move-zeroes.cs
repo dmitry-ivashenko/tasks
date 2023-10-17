47643939fcc086846311e8d27809db63ddc95c82
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-10-17 18:22:55 +0200

    Create 0283-move-zeroes.cs

---
+++ Array/0283-move-zeroes.cs
@@ -0,0 +1,29 @@
+
+// 283. Move Zeroes
+// https://leetcode.com/problems/move-zeroes/
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        int readIndex = 0;
+        int writeIndex = 0;
+
+        while (readIndex < nums.Length)
+        {
+            if (nums[readIndex] == 0)
+            {
+                readIndex++;
+                continue;
+            }
+            if (readIndex != writeIndex)
+            {
+                nums[writeIndex] = nums[readIndex];
+                nums[readIndex] = 0;
+            }
+            writeIndex++;
+            readIndex++;
+        }
+    }
+}
+        
