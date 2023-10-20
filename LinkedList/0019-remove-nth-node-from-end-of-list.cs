e06d0da1e3a35c6665342201dda3660e2d67a8df
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-10-20 16:17:36 +0200

    Create 0019-remove-nth-node-from-end-of-list.cs

---
+++ LinkedList/0019-remove-nth-node-from-end-of-list.cs
@@ -0,0 +1,37 @@
+
+// 19. Remove Nth Node From End of List
+// https://leetcode.com/problems/remove-nth-node-from-end-of-list/
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveNthFromEnd(ListNode head, int n) {
+        var dummy = new ListNode(0, head);
+        var left = dummy;
+        var right = head;
+        
+        while(n > 0) {
+            right = right.next;
+            n--;
+        }
+        
+        while(right != null) {
+            left = left.next;
+            right = right.next;
+        }
+        
+        // delete
+        left.next = left.next.next;
+        return dummy.next;
+    }
+}
+        
