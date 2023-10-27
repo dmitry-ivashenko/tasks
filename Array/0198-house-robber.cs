e5e8093d421bace05659cf9bcabe0eb35fe5ad67
Dmitrii Ivashchenko <ivaschenko.dmitrii@gmail.com>
2023-10-27 21:36:13 +0200

    Create 0198-house-robber.cs

---
+++ Array/0198-house-robber.cs
@@ -0,0 +1,18 @@
+
+// 198. House Robber
+// https://leetcode.com/problems/house-robber/
+
+public class Solution {
+    public int Rob(int[] nums) {
+        int rob1 = 0, rob2 = 0;
+        
+        foreach(var num in nums) {
+            var temp = Math.Max(num + rob1, rob2);
+            rob1 = rob2;
+            rob2 = temp;
+        }
+        
+        return rob2;
+    }
+}
+        
